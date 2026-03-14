37a3a97894049fd7f01401eed8926e4aa1b634d2
Romildo Oliveira Souza Júnior <mildo@mildo.dev>
2026-03-14 22:36:45 +0000

    fix(debian): use linux-headers-amd64 instead of linux-headers-$(uname -r)

--- src/debian/privileged.sh
+++ src/debian/privileged.sh
@@ -17,7 +17,7 @@ install_nvidia_drivers() {
     curl -fsSL https://developer.download.nvidia.com/compute/cuda/repos/debian13/x86_64/8793F200.pub | gpg --dearmor > /usr/share/keyrings/nvidia-cuda.gpg
     echo -e "Types: deb\nURIs: https://developer.download.nvidia.com/compute/cuda/repos/debian13/x86_64/\nSuites: /\nComponents:\nSigned-By: /usr/share/keyrings/nvidia-cuda.gpg" > /etc/apt/sources.list.d/nvidia-cuda.sources
     apt update
-    apt -y install "linux-headers-$(uname -r)" nvidia-kernel-open-dkms
+    apt -y install "linux-headers-amd64" nvidia-kernel-open-dkms
     mokutil --import /var/lib/dkms/mok.pub
 }
 
